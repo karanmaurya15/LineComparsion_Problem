233f85b84bfe09477b63f0d174056e1ac8e5f59e
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-22 21:24:42 +0530

    Length of line is calculated

--- LineComparsion_Problem/Program.cs
+++ LineComparsion_Problem/Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            int x1, y1, x2, y2;
+
+            Console.Write("\nEnter x1 co-ordinate: ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter y1 co-ordinate: ");
+            y1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nEnter x2 co-ordinate: ");
+            x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter y2 co-ordinate: ");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"\nlength of the Line is {lengthOfLine}");
+
         }
     }
 }
