f3e8f797eeedb81f6a1cf547e8f6620efcfd9e67
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-22 21:41:29 +0530

    Equality of lines

--- LineComparsion_Problem/Program.cs
+++ LineComparsion_Problem/Program.cs
@@ -5,23 +5,40 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("\n==========Line 1==========\n");
             int x1, y1, x2, y2;
-
-            Console.Write("\nEnter x1 co-ordinate: ");
+            Console.Write("Enter x1 co-ordinate: ");
             x1 = Convert.ToInt32(Console.ReadLine());
-
             Console.Write("Enter y1 co-ordinate: ");
             y1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("\nEnter x2 co-ordinate: ");
+            Console.Write("Enter x2 co-ordinate: ");
             x2 = Convert.ToInt32(Console.ReadLine());
-
             Console.Write("Enter y2 co-ordinate: ");
             y2 = Convert.ToInt32(Console.ReadLine());
+            double lengthOfLine1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"\nLength of the Line is {lengthOfLine1}");
 
-            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine($"\nlength of the Line is {lengthOfLine}");
+            Console.WriteLine("\n==========Line 2==========\n");
+            int a1, b1, a2, b2;
+            Console.Write("Enter a1 co-ordinate: ");
+            a1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b1 co-ordinate: ");
+            b1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a2 co-ordinate: ");
+            a2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter b2 co-ordinate: ");
+            b2 = Convert.ToInt32(Console.ReadLine());
+            double lengthOfLine2 = Math.Sqrt(Math.Pow(a2 - a1, 2) + Math.Pow(b2 - b1, 2));
+            Console.WriteLine($"\nLength of the Line is {lengthOfLine2}\n");
 
+            if (lengthOfLine1 == lengthOfLine2)
+            {
+                Console.WriteLine("The two lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("The two lines are Not Equal");
+            }
         }
     }
 }
